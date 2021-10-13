162d81c7e4d231f95d3764328d664c4ad1a2e3d8
slaakko <slaakko@gmail.com>
2021-10-13 15:03:35 +0300

    inst

--- inst/inst.bat
+++ inst/inst.bat
@@ -4,5 +4,7 @@ if NOT EXIST ..\bin mkdir ..\bin
 if NOT EXIST ..\lib mkdir ..\lib
 wnginst -v ..\x64\Debug\*.exe ..\bin
 wnginst -v ..\x64\Release\*.exe ..\bin
+wnginst -v ..\x64\Debug\*.pdb ..\bin
+wnginst -v ..\x64\Release\*.pdb ..\bin
 wnginst -v ..\x64\Debug\*.lib ..\lib
 wnginst -v ..\x64\Release\*.lib ..\lib
